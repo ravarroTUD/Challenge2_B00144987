56793cf6a3b67771014c6a3a455d7d6e3a6ec23f
ravarroTUD <b00144987@mytudublin.ie>
2022-10-14 02:51:01 +0100

    Added completed Challenge 2

---
+++ Challenge 2/Assets/Challenge 2/Scripts/Link.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class Link : MonoBehaviour 
+{
+
+	public void OpenGithub()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://github.com/ravarroTUD/Challenge2_B00144987");
+		#endif
+	}
+
+	[DllImport("__Internal")]
+	private static extern void openWindow(string url);
+
+}
---
+++ Challenge 2/Assets/Challenge 2/Scripts/OpenLink.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+    // Open Github project link
+   public void OpenGithub() {
+    Application.OpenURL("https://github.com/ravarroTUD/Challenge2_B00144987");
+   }
+}
